115c9b4382c12e9bea09870e777ff3b57f2b8599
Eridan Vilasboas <eridanvilas@gmail.com>
2021-08-03 09:15:56 -0300

    fileupload

--- src/RelatorioMensalWEB/src/app/app.module.ts
+++ src/RelatorioMensalWEB/src/app/app.module.ts
@@ -10,8 +10,13 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { NavComponent } from './components/template/nav/nav.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { HomeComponent } from './views/home/home.component';
+import { RelatorioComponent } from './views/relatorio/relatorio.component';
+import { FileToUpdateComponent } from './views/file-to-update/file-to-update.component';
+
 // import from '@angular/material/grid-list';
 
 @NgModule({
@@ -21,6 +26,8 @@ import { HomeComponent } from './views/home/home.component';
     FooterComponent,
     NavComponent,
     HomeComponent,
+    RelatorioComponent,
+    FileToUpdateComponent,
    
   ],
   imports: [
@@ -29,7 +36,9 @@ import { HomeComponent } from './views/home/home.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/RelatorioMensalWEB/src/app/views/file-to-update/file-to-update.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-file-to-update',
+  templateUrl: './file-to-update.component.html',
+  styleUrls: ['./file-to-update.component.css']
+})
+export class FileToUpdateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
