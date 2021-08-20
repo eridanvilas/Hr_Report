777989305b016605a7e5b75d5fd778ce8a008949
Eridan Vilasboas <eridan.batista@scanbrazilconsulting.com>
2021-08-20 13:44:20 -0300

    Script para criar nova tabela no SQLITE 3

---
+++ src/Relatorio_Mensal_API/Schema/Hr_Report/SchemaDB.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE HoursWorked (
+    [Id]          INTEGER PRIMARY KEY AUTOINCREMENT,
+    [Usuario]     TEXT,
+    [Data]        DATE,
+    [HoraEntrada] TEXT,
+    [HoraSaida]   TEXT,
+    [TotalHoras]  TEXT,
+    [Projeto]     TEXT,
+    [Descricao]   TEXT,
+);
