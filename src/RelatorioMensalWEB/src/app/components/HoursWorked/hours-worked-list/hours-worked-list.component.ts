9de80c3f46f8ac397cc143400a56ef112620a5c1
Eridan Vilasboas <eridan.batista@scanbrazilconsulting.com>
2021-10-29 17:54:17 -0300

    Ajuste tirar dados mocados

--- src/RelatorioMensalWEB/src/app/components/HoursWorked/hours-worked-list/hours-worked-list.component.ts
+++ src/RelatorioMensalWEB/src/app/components/HoursWorked/hours-worked-list/hours-worked-list.component.ts
@@ -10,12 +10,15 @@ import { HoursWorkedService } from 'src/app/services/hours-worked.service';
 export class HoursWorkedListComponent implements OnInit {
 
     hoursWorkeds!: HoursWorked[];
+    nameUser = "eridan.batista";
+    month = "6";
+
     displayedColumns = ['data', 'horaEntrada', 'horaSaida', 'totalHoras', 'projeto', 'descricao'];
 
     constructor(private hoursWorkedService: HoursWorkedService) { }
 
     ngOnInit(): void {
-        this.hoursWorkedService.getHoursMonthByUsuario("eridan.batista", "5").subscribe(hoursWorkeds => {
+        this.hoursWorkedService.getHoursMonthByUsuario(this.nameUser, this.month).subscribe(hoursWorkeds => {
             this.hoursWorkeds = hoursWorkeds;
         });
     }
